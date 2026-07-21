20fb0eada5d837d02fba8a405250b11de77e0e6e
sadio-kanoute <sadio.kanoute@laplateforme.io>
2026-07-21 13:51:17 +0200

    FORMULAIRE DE CONNEXION ET INSCRIPTION

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,16 @@
 import "./index.css";
 
+import { Link } from "react-router-dom";
+
 import Filters from "../Filters";
 
 export default function Header() {
   return (
     <div className="header">
+      <nav>
+        <Link to="/login">Login</Link>{" "}
+        <Link to="/register">Register</Link>
+      </nav>
       <Filters></Filters>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,8 @@ import { userSlice } from "./slices/userSlice";
 import { initializeCartThunk } from "./thunkActionsCreator/cartThunks";
 
 import Store from "./pages/Store";
+import Login from "./pages/login";
+import Register from "./pages/register";
 import Header from "./components/Header";
 import Error404 from "./pages/Error404";
 
@@ -43,6 +45,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Header />
       <Routes>
         <Route path="/" element={<Store />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </Router>
