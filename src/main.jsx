f4b358bfdfadc037cf884debd9e7bb6ebdd7421a
petit-pierre <aubree.pierre2@gmail.com>
2026-07-21 11:37:16 +0200

    V0.0.8 mentions legales

--- src/main.jsx
+++ src/main.jsx
@@ -17,12 +17,11 @@ import { initializeCartThunk } from "./thunkActionsCreator/cartThunks";
 import Store from "./pages/Store";
 import Header from "./components/Header";
 import Error404 from "./pages/Error404";
-import UserTest from "./components/UserTest";
-import MentionsLegales from "./pages/MentionsLegales/MentionsLegales";
-import CGU from "./pages/CGU/CGU";
-import CGV from "./pages/CGV/CGV";
+import MentionsLegales from "./pages/MentionsLegales";
+import CGU from "./pages/CGU";
+import CGV from "./pages/CGV";
 import User from "./pages/User";
-import Cart from "./components/Cart";
+import Cart from "./pages/Cart";
 
 const store = configureStore({
   reducer: {
@@ -49,7 +48,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Header />
       <Routes>
         <Route path="/" element={<Store />} />
-        <Route path="/test-user" element={<UserTest />} />
         <Route path="/mentions-legales" element={<MentionsLegales />} />
         <Route path="/cgu" element={<CGU />} />
         <Route path="/cgv" element={<CGV />} />
--- src/pages/MentionLegales/index.jsx
+++
@@ -1,3 +0,0 @@
-export function MentionsLegales() {
-    return ("Mentions Légales");
-}
